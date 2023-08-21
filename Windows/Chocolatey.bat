709f3fce25440a9d2c7831e12459eda6005f9139
Nairbuul <42011526+Nairbuul@users.noreply.github.com>
2023-08-20 17:40:58 -0700

    Add files via upload

---
+++ Windows/Chocolatey.bat
@@ -0,0 +1,2 @@
+REM Install Chocolatey (if needed)
+where choco.exe > nul 2>&1 || powershell.exe -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
---
+++ Windows/VSCode.bat
@@ -0,0 +1,8 @@
+REM Install Visual Studio Code text editor (if needed)
+where code.exe > nul 2>&1 || choco install vscode -y
+
+REM refreshing environment.
+call refreshenv.cmd
+
+cd /d %~dp0
+for /F "tokens=*" %%A in (name of text file .txt) do %%A
